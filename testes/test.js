073ad3a56ab84781eddd532a15d7fce8bb87befc
Bin0007 <122222621+Bin0007@users.noreply.github.com>
2023-05-16 20:59:12 -0300

    meu primeiro commit

---
+++ testes/test.js
@@ -0,0 +1,16 @@
+var nav = document.getElementById("nav");
+var showMenu = document.getElementById("showMenu");
+var hideMenu = document.getElementById("hideMenu");
+showMenu.addEventListener("click", function () {
+  nav.classList.add("show");
+});
+
+hideMenu.addEventListener("click", function () {
+  nav.classList.remove("show");
+});
+
+function exibe(){
+		
+
+    document.getElementById('nav').body= 
+}
